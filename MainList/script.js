e709f0798f6d4cb4e09d013bacc010d39202de06
hypergermented <germin0.3kin@gmail.com>
2025-09-28 13:54:50 +0200

    Update script.js

--- MainList/script.js
+++ MainList/script.js
@@ -160,6 +160,7 @@
     "../history/2025-09-11.json",
     "../history/2025-09-20.json",
     "../history/2025-09-21.json",
+    "../history/2025-09-28.json",
   ];
 
   const recentChangesEl = document.getElementById('recent-changes');
@@ -249,6 +250,7 @@
   }
 
 })();
+
 
 
 
